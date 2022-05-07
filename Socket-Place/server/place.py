c254142cde2f9002d486e9bf059f1f658766b99e
vanloc1808 <vanloc1808@gmail.com>
2022-05-07 23:08:51 +0700

    coordinate class

--- Socket-Place/server/place.py
+++ Socket-Place/server/place.py
@@ -1,17 +1,11 @@
-class Place:
-    def __init__(self, name, images_lists = []):
-        self.name = name
-        self.images_lists = images_lists
-    
-    def change_name(self, new_name):
-        self.name = new_name
+class Coordinate:
+    def __init__(self, longitude, latitude):
+        if (longitude < -180 or longitude > 180):
+            raise ValueError("Longitude must be between -180 and 180")
+        
+        if (latitude < -90 or latitude > 90):
+            raise ValueError("Latitude must be between -90 and 90")
+        
+        self.longitude = longitude
+        self.latitude = latitude
 
-p1 = Place("Ho Chi Minh City")
-print(p1.name)
-p1.change_name("Sai Gon")
-print(p1.name)
-
-l2 = ["img/img1", "img/img2", "img/img3"]
-p2 = Place("Ha Noi", l2)
-print(p2.name)
-print(p2.images_lists)
