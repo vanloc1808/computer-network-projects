6e6b6acc20382191ad8aabbba87f2bbc3280e1d2
vanloc1808 <vanloc1808@gmail.com>
2022-06-01 17:35:00 +0700

    command parsing

--- Socket-Email/server_control/handle.py
+++ Socket-Email/server_control/handle.py
@@ -10,6 +10,69 @@ def is_valid_ip(ip):
     
     return False
 
+def authorize(sender_address, ip_address, ip_per_address):
+    for d in ip_per_address:
+        if sender_address in d:
+            raise Exception("This email has already connected")
+        
+        if ip_address in d:
+            raise Exception("This IP has already connected")
+    
+    ip_dict = {sender_address: ip_address}
+    ip_per_address.append(ip_dict) 
+
+def list_ip():
+    pass
+
+def list_process():
+    pass
+
+def list_application():
+    pass
+
+def kill_process(id):
+    pass
+
+def kill_application(id):
+    pass
+
+def capture_webcam(time):
+    pass
+
+def capture_screen():
+    pass
+
+def disconnect(sender_address, ip_per_address):
+    connected = False
+    for d in ip_per_address:
+        if sender_address in d:
+            ip_per_address.remove(d)
+            connected = True
+            break
+    if connected == False:
+        raise Exception("This email has not connected")
+
+def shut_down():
+    pass
+
+def restart():
+    pass
+
+def keylog(time):
+    pass
+
+def registry_list(path_to_folder):
+    pass
+
+def registry_update(absolute_path, value):
+    pass
+
+def dir_list(path_to_folder):
+    pass
+
+def dir_copy(src_path, dst_path):
+    pass
+
 def raise_error_message(error_message):
     print(error_message)
 
--- Socket-Email/server_control/parse.py
+++ Socket-Email/server_control/parse.py
@@ -1,6 +1,12 @@
+from ipaddress import ip_address
 import socket as sk
 
 import handle
+
+# a list of dictionary, each of which has the type
+# { email_address: IP connected }
+ip_per_address = [] 
+
 
 """
     AUTH <Key> <IP>: start connection to IP, return ACK
@@ -25,7 +31,7 @@ import handle
             UPDATE <absolute path> <value>, ACK
         DIR COPY <src path> <dst path folder>, ACK
 """
-def command_parser(message):
+def command_parser(message, sender_address):
     msg = message.split(' ')
     file_in = open("keys.txt", "r")
     key = file_in.readline()
@@ -36,17 +42,100 @@ def command_parser(message):
     try:
         if len(msg) > 4: # too many arguments
             raise Exception("Too many arguments")
+        if len(msg) == 0:
+            raise Exception("No arguments")
         
-        if len(msg) == 3:
-            if (msg[0] != "AUTH"):
+        if len(msg) == 4:
+            if msg[0] == "REGISTRY":
+                if msg[1] == "UPDATE":
+                    handle.registry_update(msg[2], msg[3])
+                else:
+                    raise Exception("Invalid command")
+
+            elif msg[0] == "DIR":
+                if msg[1] == "UPDATE":
+                    handle.registry_update(msg[2], msg[3])
+                elif msg[1] == "COPY":
+                    handle.dir_copy(msg[2], msg[3])
+                else:
+                    raise Exception("Invalid command")
+
+            else:
                 raise Exception("Invalid command")
-            
-            if (msg[1] != key):
-                raise Exception("Invalid key")
-            
-            if (handle.is_valid_ip(msg[2]) == False):
-                raise Exception("Invalid IP")
-            
+        
+        elif len(msg) == 3:
+            if msg[0] == "AUTH":
+                if msg[1] != key:
+                   raise Exception("Invalid key")
+
+                if handle.is_valid_ip(msg[2]) == False:
+                    raise Exception("Invalid IP")  
+
+                 # if the code goes here, it will start connection to the IP at msg[2]
+                ip_address = msg[2]
+                handle.authorize(sender_address, ip_address, ip_per_address)
+
+            elif msg[0] == "REGISTRY":
+                if msg[1] == "LIST":
+                    handle.registry_list(msg[2])
+                else:
+                    raise Exception("Invalid command")
+
+            elif msg[0] == "DIR":
+                if msg[1] == "LIST":
+                    handle.dir_list(msg[2])
+                else:
+                    raise Exception("Invalid command")
+
+            else:
+                raise Exception("Invalid command")
+
+        elif len(msg) == 2:
+            if msg[0] == "LIST":
+                if msg[1] != key:
+                    raise Exception("Invalid key")
+
+                handle.list_ip()
+            elif msg[0] == "KILL":
+                list_of_process = handle.list_process()
+                if msg[1] in list_of_process:
+                    handle.kill_process(msg[1])
+                else:
+                    list_of_application = handle.list_application()
+                    if msg[1] in list_of_application:
+                        handle.kill_application(msg[1])
+                    else:
+                        raise Exception("Invalid PID or ID")
+            elif msg[0] == "WEB" or msg[0] == "REC":
+                if msg[1].isdigit() == False:
+                    raise Exception("Invalid number of seconds")
+                else:
+                    handle.capture_webcam(int(msg[1]))
+            elif msg[0] == "KEYLOG":
+                if msg[1].isdigit() == False:
+                    raise Exception("Invalid number of seconds")
+                else:
+                    handle.keylog(int(msg[1]))
+            else:
+                raise Exception("Invalid command")
+        else:
+            if msg[0] == "DISC": # disconnect 
+                handle.disconnect(sender_address, ip_per_address)
+            elif msg[0] == "LIST_PROC":
+                handle.list_process()
+            elif msg[0] == "LIST_ABP":
+                handle.list_application()
+            elif msg[0] == "SCREENSHOT":
+                handle.capture_screen()
+            elif msg[0] == "SHUTDOWN":
+                handle.shutdown()
+                handle.disconnect(sender_address, ip_per_address)
+            elif msg[0] == "RESTART":
+                handle.restart()
+                handle.disconnect(sender_address, ip_per_address)
+            else:
+                raise Exception("Invalid command")
+                
 
 
     except Exception as e:
@@ -69,6 +158,37 @@ def main():
     # command_parser(msg)
     # msg = "AUTH ABDCFE123465 255.255.255.255:65536"
     # command_parser(msg)
+    """
+    print(ip_per_address)
+    msg = "AUTH ABDCFE123465 192.168.1.0:254"
+    email = "vanloc1808@gmail.com"
+    command_parser(msg, email)
+    print(ip_per_address)
+    msg = "AUTH ABDCFE123465 255.255.255.255:65535"
+    command_parser(msg, email)
+    print(ip_per_address)
+    msg = "AUTH ABDCFE123465 127.0.0.1:1"
+    email = "vanloc1808o19@gmail.com"
+    command_parser(msg, email)
+    print(ip_per_address)
+    
+    t1 = (1, 2, 3)
+    print(t1[0])
+
+    t2 = (4, 5, 6)
+
+    t = [t1, t2]
+    print(t)
+
+    for i in t:
+        print(i[0])
+    
+    print("16".isdigit())
+    print("-1".isdigit())
+    """
+
+
+
     print("End main")
 
 main()
