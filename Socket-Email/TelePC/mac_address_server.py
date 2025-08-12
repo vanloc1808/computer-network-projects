e92673aca1406400166c784bb2709347a43d1fb0
Van-Loc Nguyen <vanloc1808@gmail.com>
2025-08-12 13:35:28 +0700

    chore: remove legacy directories Socket-Place and Socket-Email (moved to new packages)

--- Socket-Email/TelePC/mac_address_server.py
+++
@@ -1,4 +0,0 @@
-import uuid
-def mac_address(client):
-    client.sendall(bytes(hex(uuid.getnode()), "utf8"))
-    return
--- Socket-Email/server_control/old_handle/shutdown_logout_server.py
+++
@@ -1,8 +0,0 @@
-def shutdown(conn):
-    conn.sendall(bytes("SHUTDOWN", "utf8"))
-
-def logout(conn):
-    conn.sendall(bytes("LOGOUT", "utf8"))
-
-def restart(conn):
-    conn.sendall(bytes("RESTART", "utf8"))
